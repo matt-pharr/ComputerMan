0f217d8adacfec67c60a9226b6ba9783e041d6f6
Rose <matthewcpharr@gmail.com>
2020-12-11 02:07:25 -0500

    bot init

---
+++ discord-bot/bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+
+client.run(TOKEN)
